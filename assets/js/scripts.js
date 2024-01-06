ebe304e7a7d9f12c8164c6b0824dcc82ea703e43
yosephdev <>
2024-01-06 18:55:17 +0000

    feat: Implement hamburger menu toggle and smooth scrolling for internal links

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,4 +5,21 @@ document.addEventListener('DOMContentLoaded', () => {
     menuToggle.addEventListener('click', () => {
         mainNav.classList.toggle('hidden');
     });
+
+    document.querySelectorAll('#main-nav a').forEach(link => {
+        link.addEventListener('click', function (event) {
+
+            if (this.getAttribute('href').startsWith('#')) {
+                event.preventDefault();
+
+                const targetId = this.getAttribute('href');
+                const targetSection = document.querySelector(targetId);
+
+                targetSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+                mainNav.classList.add('hidden');
+            }
+        });
+    });
 });
+
