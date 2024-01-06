b360cd9c73f920afc000fd9a77003c799bd82534
yosephdev <>
2024-01-06 13:51:52 +0000

    feat: Implement toggle functionality for mobile menu

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const mainNav = document.getElementById('main-nav');
+
+    menuToggle.addEventListener('click', () => {
+        mainNav.classList.toggle('hidden');
+    });
+});
